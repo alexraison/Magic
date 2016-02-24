4ec6a29121729acc2d9d2001e7307b61e1a2a064
Mike Burns <burns.23@hotmail.co.uk>
2016-02-24 11:46:06 +0000

    Try it aaaaaaaout

--- app/post.py
+++ app/post.py
@@ -68,7 +68,7 @@ class slack_bot:
                 },
                 {
                     'title': "Game Win %",
-                    'value': percentage,
+                    'value': '     ' + percentage,
                     'short': "true"
                 }               
 
