ccd18841993b254fea5ecfae8ce42f5a8172c9af
burns238 <burns.23@hotmail.co.uk>
2016-03-06 19:49:49 +0000

    coding while watching Zoolander with my cat

--- app/pairings.py
+++ app/pairings.py
@@ -137,7 +137,7 @@ def getTwoHeadedMatches(playerList):
 
 	matchList = []
 
-	twoHeadedsql = """SELECT t.name, p1.name, p2.name, p3.name, p4.name, t.date
+	sql = """SELECT t.name, p1.name, p2.name, p3.name, p4.name, t.date
 				FROM match AS m
 				INNER JOIN match_participant AS mp1 ON m.id = mp1.match_id
 				INNER JOIN match_participant AS mp2 ON m.id = mp2.match_id AND mp1.entity_id <> mp2.entity_id
