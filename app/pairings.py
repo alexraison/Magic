433a19df4c6eb0f066ec63e284674963a8155902
burns238 <burns.23@hotmail.co.uk>
2016-03-07 07:27:46 +0000

    a

--- app/pairings.py
+++ app/pairings.py
@@ -112,7 +112,6 @@ def getPotentialPairings(matchPairings, r):
 				seen.append(player)
 		if len(seen) == len(allPlayers):
 			outputPairings.append(pairings)
-	print(outputPairings)
 
 	return outputPairings
 
