30ea801c4817c93fe3225cc1fb8998fd66ff613a
burns238 <burns.23@hotmail.co.uk>
2017-04-15 18:16:40 +0100

    Automaton

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
-import app.automatePairings
+import app.automatedPairings
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
